c961e77641ce0b64a60faf17fe2e7f93b8288780
RichardZilinec <zilinecrichard@gmail.com>
2025-09-24 09:39:48 +0200

    Add files via upload

---
+++ du 1.py
@@ -0,0 +1,9 @@
+a = int(input("zadaj cislo a:"))
+b = int(input("zadaj cislo b:"))
+s = a*b
+if (s>100):
+    print ("nasobok cisel a,b je vacsi ako 100",s)
+else:
+    print ("nasobok cisel a,b je mensi ako 100",s)
+if (s==100):
+    print ("nasobok cisel je 100:",s)
---
+++ du2.py
@@ -0,0 +1,6 @@
+a = int(input("zadaj cislo a:"))
+if a == 0:
+    print ("cislo nemoze bzýt 0")
+else:
+    s = 1/a
+    print ("prevratena hodnota cisla a je:",s)

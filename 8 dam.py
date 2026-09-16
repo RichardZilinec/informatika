9fbe308eb07d90796e8e218624ab2503fca0dba0
RichardZilinec <zilinecrichard@gmail.com>
2026-09-16 17:23:32 +0200

    Refactor chessboard and image creation logic
    Refactor chessboard visualization and improve image creation.

--- 8 dam.py
+++ 8 dam.py
@@ -1,36 +1,10 @@
-import PIL
-from PIL import Image,ImageDraw
+from PIL import Image, ImageDraw
+from pathlib import Path
 chessboard = []
+#
+#
+#
 counter = 0
-size_sq = 50
-number = 8
-width = size_sq * number
-height = size_sq * number
-col_b =  (0, 0, 0)
-col_w = (255, 255, 255)
-#
-#
-#
-def createImage():
-    image = Image.new('RGB', (width, height))
-    draw = ImageDraw.Draw(image)
-    for riadok in range(number):
-        for stlpec in range(number):
-            x1 = size_sq * stlpec
-            y1 = size_sq * riadok
-            x2 = x1 + size_sq
-            y2 = y1 + size_sq
-            if (riadok + stlpec) % 2 == 1:
-                color = col_w
-            else:
-                color = col_b
-            draw.rectangle([x1,y1,x2,y2], fill=color)
-    #image.save('sachovnica.png')
-    image.show()
-createImage()
-#
-#
-#
 def create_board ():
     global chessboard
     #chessboard = [row] * 8  # mega pruser takto to nikdy nerob !!!!!
@@ -58,22 +32,51 @@ def check_it (x, y):
 #
 #
 #
-def queens (n):
-    global chessboard , counter
-    if n == 8:
-        counter += 1
-        print(chessboard)
+def createImage(counter=0):
+    size = 80
+    boardsize = 8 * size
+    img = Image.new("RGB", (boardsize, boardsize), "white")
+    draw = ImageDraw.Draw(img)
+    for y in range(8):
+        for x in range(8):
+            color = "black" if (x + y) % 2 == 0 else "white"
+            draw.rectangle(
+                [x * size, y * size, (x + 1) * size, (y + 1) * size],
+                fill=color,
+            )
+    for y in range(8):
+        for x in range(8):
+            if chessboard[y][x] == 1:
+                draw.ellipse(
+                    [
+                        x * size + 10,
+                        y * size + 10,
+                        (x + 1) * size - 10,
+                        (y + 1) * size - 10,
+                    ],
+                    fill="green",
+                )
+    img.show()
+#
+#
+#
+def queens(n):
+    global chessboard
+    global counter
+    if n==8:
+        counter+=1
+        print (f'Riesenie c. {counter}')
+        createImage(counter)
+        print("---------------------------------------------")
         print(counter)
-        print('-----------------------------')
-    else:
-        for i in range (8):
+    else :
+        for i in range(0,8):
             if check_it(i,n):
-                chessboard[n][i] = 1
+                chessboard[n][i]=1
                 queens(n+1)
-                chessboard[n][i] = 0
-#
-#
-#
+                chessboard[n][i]=0
+
 create_board()
+#
 queens(0)
-#print(check_it(4,3))
+
